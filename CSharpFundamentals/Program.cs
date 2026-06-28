54a88c1e9a26fca5809ca169b58aac31a2e32610
ma7fouz81 <amedmafouz53@gmail.com>
2026-06-28 05:45:33 +0300

    Add project files.

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharpFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Title = "C# Fundamentals | Passionate Coders";
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.Write("Hello");
+            //Console.BackgroundColor = ConsoleColor.Yellow;
+            //Console.Write(", World!");
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.BackgroundColor = ConsoleColor.Black;
+            //Console.ReadKey();
+
+            Console.Title = "Ahmed Mahfouz";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello Ahmed");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello Mahfouz");
+            Console.ReadKey();
+        }
+    }
+}
