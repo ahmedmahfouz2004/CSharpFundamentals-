97ca6428aed3032d6a798ea01a33aad75cb351c8
ma7fouz81 <amedmafouz53@gmail.com>
2026-06-28 07:50:26 +0300

    Char Data Type

---
+++ CSharp Char Data Type/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp_Char_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = 'A';
+            Console.WriteLine(c);
+            Console.WriteLine("=====================");
+            Console.WriteLine((int)c);
+            Console.WriteLine("=====================");
+            bool IsDigit = char.IsDigit(c);
+            Console.WriteLine(IsDigit);
+            Console.WriteLine("=====================");
+            bool IsLetter = char.IsLetter(c);
+            Console.WriteLine(IsLetter);
+
+        }
+    }
+}
