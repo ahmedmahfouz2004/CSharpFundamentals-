4ccd54cf0de98a26cfd8ca898b6117b7ef8df8a4
ma7fouz81 <amedmafouz53@gmail.com>
2026-06-29 05:54:38 +0300

    Parse String

---
+++ CSharp - Parse String/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharp___Parse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your birth year: ");
+
+            string year = Console.ReadLine();
+            int intYear = int.Parse(year);
+
+            //double doubleYear = double.Parse(year);
+
+            Console.WriteLine($"Your age until 2026 = {2026 - intYear}");
+
+            //Console.WriteLine($"Your age until 2026 = {2026 - doubleYear}");
+        }
+    }
+}
