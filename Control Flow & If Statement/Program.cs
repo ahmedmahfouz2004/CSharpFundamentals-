b82ce9772d361a8ea5e22954f6b6ceb05e161d39
ma7fouz81 <amedmafouz53@gmail.com>
2026-06-29 07:10:16 +0300

    If Statement

---
+++ Control Flow & If Statement/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Control_Flow___If_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter a number: ");
+            string number = Console.ReadLine();
+            int parse_number = int.Parse(number);
+
+            if (parse_number % 2 == 0 && parse_number != 0)
+            {
+                Console.WriteLine($"{parse_number} is even");
+            }
+
+            else if (parse_number == 0)
+            {
+                Console.WriteLine("Zero is neither odd nor even.");
+            }
+            
+            else 
+            {
+                Console.WriteLine($"{parse_number} is Odd");
+            }
+
+        }
+    }
+}
