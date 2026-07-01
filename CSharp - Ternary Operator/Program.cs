926805424f609c4ce07dc169a88747de412c9b2b
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-01 05:29:58 +0300

    Ternary Operator

---
+++ CSharp - Ternary Operator/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharp___Ternary_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i<=10; i++)
+            {
+                if(i %2 == 0)
+                {
+                    Console.WriteLine($"{i} is even");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is Odd");
+                }
+            }
+            Console.WriteLine("======================");
+
+            // using ternary operator
+            for (int i=1; i<=10; i++) 
+            {
+                string statement = (i % 2 == 0 ? $"{i} is even" : $"{i} is Odd");
+
+                Console.WriteLine(statement);
+            }
+            
+        }
+    }
+}
