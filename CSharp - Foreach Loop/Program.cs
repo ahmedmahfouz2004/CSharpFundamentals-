41e587e024ea20c02f9faa1822670985ac9b2b0a
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-01 05:52:18 +0300

    Foreach Loop

---
+++ CSharp - Foreach Loop/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharp___Foreach_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = { 'a', 'b', 'c', 'd', 'e', 'f' };
+            for (int i = 0; i < chars.Length; i++)
+            {
+                Console.WriteLine($"{chars[i]} = {(int)(chars[i])}");
+            }
+            Console.WriteLine("=================");
+            int[] numbers = { 99 , 58 ,85 , 90 , 60 };
+            foreach(int number in numbers)
+            {
+                Console.WriteLine($"{number} : {(char)(number)}");
+            }
+
+        }
+    }
+}
