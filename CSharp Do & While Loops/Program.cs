108d2bf1afe4f7099754d923894d29fb99614b27
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-03 09:49:04 +0300

    Do & While Loops

---
+++ CSharp Do & While Loops/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CSharp_Do___While_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter any string to capitalize: ");
+                string input = Console.ReadLine();
+
+                if(input != "exit")
+                {
+                    Console.WriteLine(input.ToUpper());
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            //Console.WriteLine("==============");
+            //int x = 5;
+            //do
+            //{
+            //    Console.WriteLine(x++);
+            //}
+            //while (x <= 10);
+
+            //Console.WriteLine("==============");
+
+            //int y = 5;
+            //while (y <= 10)
+            //{
+            //    Console.WriteLine(y++);
+            //}
+        }
+    }
+}
