a57cc3059033d53d1d0e4ae2f6fc93aba389bf72
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-03 09:31:36 +0300

    For & Foreach Training

---
+++ CSharp - For & Foreach Training/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp___For___Foreach_Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter numbers of array : ");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] array = new int[arraySize];
+            for(int i = 0; i < arraySize; i++)
+            {
+                Console.Write($"enter numbers of array {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int[] reversedArray = new int[arraySize];
+            for (int i = arraySize-1; i>= 0; i--)
+            {
+                reversedArray[arraySize -i -1] = array[i];
+            }
+            foreach (int number in array)
+                Console.WriteLine(number);
+
+            Console.WriteLine("--------------------");
+
+            foreach (int number in reversedArray)
+                Console.WriteLine(number);
+
+            
+        }
+    }
+}
