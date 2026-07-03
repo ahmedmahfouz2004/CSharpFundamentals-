3e9f097dac718802eb17abaff74f01041397c68e
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-03 11:41:05 +0300

    Methods - Part 2

--- Csharp - Methods/Program.cs
+++ Csharp - Methods/Program.cs
@@ -4,19 +4,29 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6 };
-            CalculateAverage(numbers);
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            CalculateAverage(numbers, printSumToConsole: true);
+
+            numbers = new int[] { 10, 20, 30, 40, 50 };
+            CalculateAverage(numbers, true);
         }
 
-        static void CalculateAverage (int[] numbers)
+        static double CalculateAverage(int[] numbers,bool printAverageToConsole = false, bool printSumToConsole = false)
         {
             int sum = 0;
+
             foreach (int number in numbers)
-            {
                 sum += number;
-            }
+
             double average = sum / numbers.Length;
-            Console.WriteLine (average);
+
+            if (printSumToConsole == true)
+                Console.WriteLine("Sum = " + sum);
+
+            if (printAverageToConsole == true)
+                Console.WriteLine($"Average = {average}");
+
+            return average;
         }
     }
 }
