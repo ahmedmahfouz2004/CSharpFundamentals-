56ac6fa16790474212c46725dfce580172839cbd
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-03 11:24:00 +0300

    Methods - Part 1

---
+++ Csharp - Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Csharp___Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+            CalculateAverage(numbers);
+        }
+
+        static void CalculateAverage (int[] numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            double average = sum / numbers.Length;
+            Console.WriteLine (average);
+        }
+    }
+}
