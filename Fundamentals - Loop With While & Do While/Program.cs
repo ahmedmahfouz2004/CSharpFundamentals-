6f6f14fb6cca962cd8e2a8dc59ed29de8e41a819
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-04 19:36:02 +0300

    While_Do_While

---
+++ Fundamentals - Loop With While & Do While/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Fundamentals___Loop_With_While___Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 5)
+            {
+                Console.WriteLine(i); 
+                if (i == 2)
+                {
+                    break;
+                }
+                i++;
+            }
+            Console.WriteLine("=================");
+            int index = 2;
+
+            do
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index < 4);
+
+        }
+    }
+}
