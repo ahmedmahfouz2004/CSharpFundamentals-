3b9dcb16c92bfbca36754a1e9f24a740ebb92a78
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-04 19:47:11 +0300

    Break_Continue

---
+++ Loop - Break, Continue/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Loop___Break__Continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 20, 30, 40, 50 };
+
+            for(int i=0; i<nums.Length; i++)
+            {
+                if (nums[i] == 20) 
+                {
+                    continue;
+                }
+                Console.WriteLine(nums[i]);
+            }
+            Console.WriteLine("===================");
+            int[] numbers = { 10, 20, 30, 40, 50 };
+
+            for(int i=0; i< numbers.Length; i++)
+            {
+                if (numbers[i] == 30) 
+                {
+                    break;
+                }
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
