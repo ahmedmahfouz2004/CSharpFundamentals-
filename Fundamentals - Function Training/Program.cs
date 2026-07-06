702a81d2e823d579ba3f6c9665e3f84b4d952eb1
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-06 15:02:08 +0300

    Function Training

---
+++ Fundamentals - Function Training/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Fundamentals___Function_Training
+{
+    internal class Program
+    {
+        static void IceBox(string Item)
+        {
+            if(Item == "Apple")
+            {
+                Console.WriteLine("Will Be More Ice");
+            }
+            else if(Item == "Coca Cola")
+            {
+                Console.WriteLine("Will Be More Fresh");
+            }
+            else if(Item == "Juice")
+            {
+                Console.WriteLine("Will Be More Good");
+            }
+            else
+            {
+                Console.WriteLine("This Item Is Invalid");
+            }
+        }
+
+        static void calc(int num1 ,  int num2)
+        {
+            Console.Write($"{num1} + {num2} = ");
+            Console.WriteLine(num1 + num2);
+        }
+        static void Main(string[] args)
+        {
+            IceBox("Apple");
+            IceBox("Coca Cola");
+            IceBox("Juice");
+            IceBox("Tea");
+            Console.WriteLine("==================");
+            calc(10, 20);
+        }
+    }
+}
