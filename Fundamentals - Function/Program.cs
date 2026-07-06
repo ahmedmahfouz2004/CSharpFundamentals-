91ee982960585d8a53d4598caa1c4bb50d0faedb
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-06 14:51:53 +0300

    Function

---
+++ Fundamentals - Function/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Specialized;
+
+namespace Fundamentals___Function
+{
+    internal class Program
+    {
+        static void sayHello(string msg ,string name , int age)
+        {
+            Console.WriteLine($"{msg} {name}, ");
+            Console.WriteLine($"{name}\'s Age Is: {age}");
+        }
+        static void Main(string[] args)
+        {
+            sayHello("Hello","Ahmed", 40);
+            sayHello("Hello","Mohammed", 40);
+            sayHello("Hello","Mahmoud", 40);
+        }
+    }
+}
