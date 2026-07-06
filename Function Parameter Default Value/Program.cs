11c9a04abdba657e2d8de4b437ea1536491e857f
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-06 19:08:00 +0300

    Function_Parameter_Default_Value

---
+++ Function Parameter Default Value/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Function_Parameter_Default_Value
+{
+    internal class Program
+    {
+        static void details(string msg = "Welcome" , string name = "UnKnown")
+        {
+            Console.WriteLine($"{msg} {name}");
+        }
+        static void Main(string[] args)
+        {
+            details("Hi","Ahmed");
+            details("Hi");
+            details();
+        }
+    }
+}
