7af78bde175408ac36bea973b2a553569b968f6d
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-06 19:22:05 +0300

    Passing_Array_As_Parameter

---
+++ Function - Passing Array As Parameter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Function___Passing_Array_As_Parameter
+{
+    internal class Program
+    {
+        static void calculate(int[] nums , int count)
+        {
+            int result = 0; 
+            for(int i=0; i<nums.Length; i++)
+            {
+                result += nums[i];
+            }
+            Console.WriteLine($"Result Is: {result}");
+        }
+        static void Main(string[] args)
+        {
+            int[] arrauOfNumbers = { 10 ,20 ,30 ,40 ,50};
+            int sizeofarray = arrauOfNumbers.Length;
+            calculate(arrauOfNumbers , sizeofarray);
+        }
+    }
+}
