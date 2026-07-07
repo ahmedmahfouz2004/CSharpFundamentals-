d119ca6045d89af34a8fd267d8150775a4497bf2
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-07 05:45:30 +0300

    Return & Void

---
+++ Function - Return Statement + Void/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Function___Return_Statement___Void
+{
+    internal class Program
+    {
+        static int calc(int num1 ,int num2)
+        {
+            Console.WriteLine("Operation Is Done");
+            return num1 + num2;
+        }
+        
+        static void calculate(int num1 ,int num2)
+        {
+            Console.WriteLine($"{num1 + num2}");
+            
+        }
+        static void Main(string[] args)
+        {
+            int result = calc(10, 20);
+            Console.WriteLine($"Result Is: {result}");
+
+            Console.WriteLine("====================");
+            calculate(10 , 30);
+        }
+    }
+}
+
+
+
+
