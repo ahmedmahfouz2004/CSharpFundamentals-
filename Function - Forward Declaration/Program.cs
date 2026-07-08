54f7fde51d37d151080819f576368278e3d611b7
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-08 11:51:41 +0300

    Overloading Function

---
+++ Function - Forward Declaration/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Function___Forward_Declaration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(2, 3));
+            Console.WriteLine(Math.Abs(-30));
+            Console.WriteLine(Math.Abs(-30));
+
+        }
+    }
+}
---
+++ Function Overloading/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Function_Overloading
+{
+    internal class Program
+    {
+        static void print(int num1 , int num2)
+        {
+            Console.WriteLine($"number one is: {num1}");
+            Console.WriteLine($"number two is: {num2}");
+        } 
+        static void print(int num1 , int num2 , int num3)
+        {
+            Console.WriteLine($"number one is: {num1}");
+            Console.WriteLine($"number two is: {num2}");
+            Console.WriteLine($"number three is: {num3}");
+        }  
+        static void print(string textOne , string textTwo)
+        {
+            Console.WriteLine($"textOne is: {textOne}");
+            Console.WriteLine($"textTwo is: {textTwo}");
+        }
+        static void Main(string[] args)
+        {
+            print(10, 20);
+            print(10, 20 , 30);
+            print("Ahmed", "Mahfouz");
+        }
+    }
+}
