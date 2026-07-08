cd3b157e4b6db79a24f4f2ce6e70a0cd5a1a36cb
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-08 12:05:51 +0300

    Recursion Function

---
+++ Function Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace Function_Recursion
+{
+    internal class Program
+    {
+        static int add(int x)
+        {
+            if(x == 0)
+            {
+                return 0;
+            }
+            Console.WriteLine(x);
+            Console.WriteLine("===================");
+            return x + add(x - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(5));
+        }
+    }
+}
