1811276ff175c6c96a0909861a00af5683530375
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-09 05:22:55 +0300

    Palindrome Words

---
+++ Training Palindrome Words/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Training_Palindrome_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a word to check: ");
+            string word = Console.ReadLine().ToLower();
+
+            bool isPalindrome = false;
+
+            for(int i = 0; i< word.Length / 2; i++)
+            {
+                if(word[i] == word [word.Length - i -1] )
+                {
+                    isPalindrome =true;
+                }
+                else
+                {
+                    isPalindrome = false;
+                }
+            }
+            Console.WriteLine(isPalindrome);
+        }
+    }
+}
---
+++ Training_Array Findings/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Training_Array_Findings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Number Of Array: ");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] array = new int[arraySize];
+            for(int i = 0; i < arraySize; i++)
+            {
+                Console.Write($"enter number{i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int min = array[0];
+            int max = array[0];
+            int sum = array[0];
+
+            for (int i = 1;i< arraySize;i++)
+            {
+                sum += array[i];
+
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine($"Sum is: {sum}");
+            Console.WriteLine($"Max Number is : {max}");
+            Console.WriteLine($"Min Number is: {min}");
+        }
+    }
+}
