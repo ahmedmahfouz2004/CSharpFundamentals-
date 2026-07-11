9d379c16fbbdc229b480b9936724710afd61cbe2
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-11 18:19:33 +0300

    String Split & Join

---
+++ CSharp - String Split & Join/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharp___String_Split___Join
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //join string
+            string[] words = { "Hello", "Ahmed", "Mahfouz" };
+            string statement = string.Join(" , ", words);
+            Console.WriteLine(statement);
+            Console.WriteLine("========================");
+
+            //split string
+            Console.Write("Please enter list of numbers separated by comma: ");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(",");
+            int sum = 0;
+            foreach (string number in numbers)
+                sum += int.Parse(number);
+            Console.WriteLine("Average is: " + sum / numbers.Length);
+        }
+    }
+}
