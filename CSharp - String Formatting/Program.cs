1826925cf0f99e16f1bc0f3ea1133e1157070620
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-11 18:05:16 +0300

    String Formatting

---
+++ CSharp - String Formatting/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharp___String_Formatting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello {0} My Name Is {1}";
+            Console.WriteLine(str);
+            str = string.Format(str , "Ahmed" , "Mohammed");
+            Console.WriteLine(str);
+
+            Console.WriteLine("=====================");
+
+            double salary = 10000;
+            Console.WriteLine(salary);
+            string msg = "My Salary Is {0:C2}";
+            string sal = "My Salary Is {0:N2}";
+            msg = string.Format (msg, salary);
+            sal = string.Format (sal, salary);
+            Console.WriteLine(msg); //$10,000.00
+            Console.WriteLine("=====================");
+            Console.WriteLine(sal); //10,000.00
+        }
+    }
+}
