e776e38243cae1fc4d39e0e27a28a9e5f35a53ee
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-15 14:39:31 +0300

    Constructor

---
+++ CSharp - Constructor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp___Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Ahmed" , "0122551" , "cairo");
+            Console.WriteLine(student.name);
+            Console.WriteLine(student.phone);
+            Console.WriteLine(student.address);
+          
+        }
+    }
+}
---
+++ CSharp - Constructor/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp___Constructor
+{
+    public class Student
+    {
+        public Student(string name , string phone , string address)
+        {
+            Console.WriteLine("this a new initialized constructor");
+            this.name = name;
+            this.phone = phone;
+            this.address = address;
+        }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+    }
+}
