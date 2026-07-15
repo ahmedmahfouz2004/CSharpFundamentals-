df85c6eaadbfe1fa40b231dcaf11e1d26fcfa51c
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-15 14:22:12 +0300

    Classes & Objects

---
+++ CSharp - Classes & Objects/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharp___Classes___Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            Console.Write("enter student name: ");
+            student.name = Console.ReadLine();
+            Console.Write("enter student phone number: ");
+            student.phonenumber = Console.ReadLine();
+            Console.Write("enter student Address: ");
+            student.address = Console.ReadLine();
+
+            Console.WriteLine(student.name);
+            Console.WriteLine(student.phonenumber);
+            Console.WriteLine(student.address);
+        }
+    }
+}
---
+++ CSharp - Classes & Objects/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp___Classes___Objects
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public string phonenumber { get; set; }
+        public string address { get; set; }
+    }
+}
