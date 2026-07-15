0d130648f211bd188981c31c1f2ffdfdbcb16080
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-15 16:18:40 +0300

    Properties

---
+++ CSharp - Properties/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp___Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            //student.SetName("Ali");
+            //Console.WriteLine(student.GetName());
+
+            student.name = "mahmoud";
+            Console.WriteLine(student.name);
+        }
+    }
+}
---
+++ CSharp - Properties/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp___Properties
+{
+    public class Student
+    {
+        //private string _name;
+        //public void SetName(string name)
+        //{
+        //    _name = name;
+        //}
+        //public string GetName()
+        //{
+        //    return _name;
+        //}
+
+        public string name { get; set; }
+
+        //public string name
+        //{
+        //    get
+        //    {
+        //        return name;
+        //    }
+        //    set
+        //    {
+        //        name = value;
+        //    }
+        //}
+    }
+}
