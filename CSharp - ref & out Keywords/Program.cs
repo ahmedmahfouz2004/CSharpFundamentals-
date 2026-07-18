8799e7873e91ed0523008e96969ca2dd074e073c
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-18 14:27:30 +0300

    ref & out Keywords

---
+++ CSharp - ref & out Keywords/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace CSharp___ref___out_Keywords
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    bool isSuccessful = true;
+        //    var result = Divide(10, 0, ref isSuccessful);
+        //    Console.WriteLine($"isSuccessful = {isSuccessful}");
+        //    Console.WriteLine($"result = {result}");
+        //}
+
+        //static double Divide(double number, double divisor, ref bool isSuccessful)
+        //{
+        //    if (divisor == 0)
+        //    {
+        //        Console.WriteLine("Can't divide on Zero !");
+        //        isSuccessful = false;
+        //        return 0;
+        //    }
+
+        //    isSuccessful = true;
+        //    return number / divisor;
+        //}
+
+        static void Main(string[] args)
+        {
+            TestRef();
+            TestOut();
+        }
+
+        private static void TestRef()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            bool isSuccessful = true;
+            var result = Divide(10, 0, ref isSuccessful);
+            Console.WriteLine($"isSuccessful = {isSuccessful}");
+            Console.WriteLine($"result = {result}");
+        }
+        private static void TestOut()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            bool isSuccessful = true;
+            var result = DivideOut(10, 0, out isSuccessful);
+            Console.WriteLine($"isSuccessful = {isSuccessful}");
+            Console.WriteLine($"result = {result}");
+        }
+        static double Divide(double number, double divisor, ref bool isSuccessful)
+        {
+            if (divisor == 0)
+            {
+                Console.WriteLine("Can't divide on Zero !");
+                isSuccessful = false;
+                return 0;
+            }
+
+            isSuccessful = true;
+            return number / divisor;
+        } 
+        static double DivideOut(double number, double divisor, out bool isSuccessful)
+        {
+            if (divisor == 0)
+            {
+                Console.WriteLine("Can't divide on Zero !");
+                isSuccessful = false;
+                return 0;
+            }
+
+            isSuccessful = true;
+            return number / divisor;
+        }
+    }
+}
