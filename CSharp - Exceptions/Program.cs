20e3d70581ffd090006812bf47617257d3198c4e
ma7fouz81 <amedmafouz53@gmail.com>
2026-07-18 14:59:49 +0300

    Exceptions

---
+++ CSharp - Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharp___Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Divide(10, 0));
+                Console.WriteLine(Divide(10, 2));
+                Console.WriteLine(Divide(10, 5));
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Finally I'm Done !");
+            }
+        }
+        static int Divide(int number, int divisor)
+        {
+            return number / divisor;
+        }
+    }
+}
